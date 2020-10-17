4b628579c8ca0eaa72631631a91e784e795e16d9
KonstantinKhan <khan366kos@gmail.com>
2020-10-17 09:48:11 +0500

    Fixed saving dish

--- src/main/kotlin/com/khan366kos/serverrationcalculation/controllers/DishesController.kt
+++ src/main/kotlin/com/khan366kos/serverrationcalculation/controllers/DishesController.kt
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.MediaType
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.*
+import javax.servlet.http.HttpServletRequest
 
 @Controller
 class DishesController {
@@ -33,19 +34,25 @@ class DishesController {
     @JsonView(View.REST::class)
     @RequestMapping("/dishes/all", produces = [MediaType.APPLICATION_JSON_VALUE])
     @ResponseBody
-    fun getAllDishJSON(): List<Dish> {
+    fun getAllDishJSON(@RequestHeader("Authorization") header: String): List<Dish> {
+
+        println(jwtProvider.getLoginFromToken(header.substring(7)))
+
         val dishes = dishesRepository.findAll()
         return dishesRepository.findAllDishesUser(jwtProvider.login).toList()
     }
 
     @RequestMapping("/dish/save", method = [RequestMethod.POST], produces = [MediaType.APPLICATION_JSON_VALUE])
     @ResponseBody
-    fun saveDish(@RequestBody dish: Dish) {
+    fun saveDish(@RequestBody dish: Dish, @RequestHeader header: RequestHeader) {
+
         val user = userRepository.findByLogin(jwtProvider.login)
         dish.user = user
+        println(dish.dishId)
         dishesRepository.save(dish)
         dish.dish_product.forEach { it.dish = dish }
         dishesRepository.save(dish)
+
     }
 //
 //    @RequestMapping("/dishes/search/{name}", produces = [MediaType.APPLICATION_JSON_VALUE])
